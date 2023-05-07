8585f0159e70c8cbd5d831d240017440460be6b7
Vladi Bilonenko <bilonenko.v@gmail.com>
2023-05-07 09:58:49 +0200

    Reproduce update dataset issue

---
+++ examples/update-dataset/src/data/sample-data-2.js
@@ -0,0 +1,96 @@
+// Copyright (c) 2021 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+export default {
+  info: {
+    label: 'San Francisco Trees',
+    id: 'tree_data'
+  },
+  data: {
+    fields: [
+      {
+        name: 'TreeID'
+      },
+      {
+        name: 'Species'
+      },
+      {
+        name: 'Address'
+      },
+      {
+        name: 'Has_Species'
+      },
+      {
+        name: 'SiteInfo'
+      },
+      {
+        name: 'PlantType'
+      },
+      {
+        name: 'PlantDate'
+      },
+      {
+        name: 'Plan'
+      },
+      {
+        name: 'Age'
+      },
+      {
+        name: 'DBH'
+      },
+      {
+        name: 'Location_latitude'
+      },
+      {
+        name: 'Location_longitude'
+      }
+    ],
+    rows: [
+      [
+        141565,
+        'Myoporum laetum :: Myoporum',
+        '501X Baker St',
+        false,
+        'Sidewalk: Curb side : Cutout',
+        'Tree',
+        '7/21/88 0:00',
+        1988,
+        29,
+        21,
+        37.77596769,
+        -122.4413967
+      ],
+      [
+        232565,
+        'Metrosideros excelsa :: New Zealand Xmas Tree',
+        '940 Elizabeth St',
+        false,
+        'Sidewalk: Curb side : Yard',
+        'Tree',
+        '3/20/17 0:00',
+        2017,
+        0,
+        3,
+        37.75171022,
+        -122.441498
+      ]
+    ]
+  }
+};
